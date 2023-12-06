1571d0066e77746b25f381a71a3773bd425d021d
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-12-06 12:53:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
+
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder
 import seaborn as sns
 import plotly.express as px
+from sklearn.model_selection import train_test_split
 
 from sklearn.model_selection import train_test_split
 
